abcb4fb8f1d8f3fed0075915c5ded2cc19984437
mtuan1812 <ngmtuan1812@gmail.com>
2026-04-16 02:44:19 -0400

    aaa

--- HMI_Plot_V1/web_server.py
+++ HMI_Plot_V1/web_server.py
@@ -79,7 +79,8 @@ def make_handler(runtime):
             qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=2)
             qr.add_data(url)
             qr.make(fit=True)
-            qr_img = qr.make_image(fill_color="black", back_color="white")
+            qr_wrapper = qr.make_image(fill_color="black", back_color="white")
+            qr_img = getattr(qr_wrapper, "get_image", lambda: qr_wrapper)().convert("RGB")
             
             width, height = 520, 520
             img = Image.new("RGB", (width, height), "white")
