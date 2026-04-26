b161932e0d7f42029a62eb6660109284c89ebeaf
mtuan1812 <ngmtuan1812@gmail.com>
2026-04-25 22:53:59 -0400

    aaaab

--- HMI/app_state.py
+++ HMI/app_state.py
@@ -334,9 +334,10 @@ class RuntimeState:
             elif action == "toggle_dim":
                 self.log("Putting display to sleep (wake on touch)...")
                 import subprocess
-                # Allow xset to turn off the screen via DPMS. A touch event will automatically wake it.
-                subprocess.Popen(["sudo", "-u", "admin", "env", "DISPLAY=:0", "XAUTHORITY=/home/admin/.Xauthority", "xset", "+dpms"])
-                subprocess.Popen(["sudo", "-u", "admin", "env", "DISPLAY=:0", "XAUTHORITY=/home/admin/.Xauthority", "xset", "dpms", "force", "off"])
+                # Use Wayland's wlopm to turn off the display (xset DPMS is unsupported on Wayland).
+                # A touch event will automatically wake it via the Wayfire compositor.
+                env_vars = {"WAYLAND_DISPLAY": "wayland-1", "XDG_RUNTIME_DIR": "/run/user/1000"}
+                subprocess.Popen(["sudo", "-u", "admin", "wlopm", "--off", "*"], env=env_vars, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
             elif action == "toggle_lock":
                 self.state.locked_controls = not self.state.locked_controls
             self._refresh_status_strings()
